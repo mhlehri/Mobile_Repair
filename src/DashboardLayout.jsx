ee8d8aa07c4df51b667b72ce37c8bc91fcfd2e16
mhlehri <lehrimirza101@gmail.com>
2023-10-24 23:29:58 +0600

    lrhipush

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -3,11 +3,11 @@ import { DashNav } from "./Components/AdminComponent/DashNav/DashNav";
 
 const Dashboard = () => {
   return (
-    <div className="flex gap-2 ">
-     <div className="sticky top-0 h-screen ">
-          <DashNav />
-     </div>
-      <Outlet></Outlet>
+    <div className="flex gap-2 relative z-50">
+      <div className="sticky top-0  z-50">
+        <DashNav />
+      </div>
+      <Outlet className="z-10"></Outlet>
     </div>
   );
 };
