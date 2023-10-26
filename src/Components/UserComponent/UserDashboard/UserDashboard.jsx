015d66364e34ce0d21264e5e2130cafe47ef5893
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-10-26 21:09:10 +0600

    making some changes on UserDashBoard

--- src/Components/UserComponent/UserDashboard/UserDashboard.jsx
+++ src/Components/UserComponent/UserDashboard/UserDashboard.jsx
@@ -1,10 +1,12 @@
 import {
   Card,
   CardBody,
-  CardFooter,
+  Step,
   Typography,
-  Button,
 } from "@material-tailwind/react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar } from "react-step-progress-bar";
+
 
 import { LineChart as LC, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
@@ -13,33 +15,59 @@ const UserDashboard = () => {
   return (
     <div className="bg-transparent container mx-auto ">
       {/* time Progress section */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 lg:gap-6">
-        <Card className="">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2">
-              Start
-            </Typography>
-            <Typography>The place is close to</Typography>
-          </CardBody>
-        </Card>
-        <Card className="">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2">
-              on process
-            </Typography>
-            <Typography>The place is close to</Typography>
-          </CardBody>
-        </Card>
-        <Card className="">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2">
-              Ready for receive
-            </Typography>
-            <Typography>The place is close to</Typography>
-          </CardBody>
-        </Card>
-      </div>
 
+      <div className="z-50 p-20 bg-white my-10 rounded-lg ">
+        <ProgressBar
+          filledBackground="linear-gradient(to right, #64CCC5, #D0E7D2)"
+          percent={75}
+        >
+          <Step transition="scale">
+            {({ accomplished, index }) => (
+              <div
+                className={`transitionStep ${
+                  accomplished ? "accomplished" : null
+                }`}
+              >
+                🌑
+              </div>
+            )}
+          </Step>
+          <Step transition="scale">
+            {({ accomplished, index }) => (
+              <div
+                className={`transitionStep ${
+                  accomplished ? "accomplished" : null
+                }`}
+              >
+                🌒
+              </div>
+            )}
+          </Step>
+          <Step transition="scale">
+            {({ accomplished, index }) => (
+              <div
+                className={`transitionStep  ${
+                  accomplished ? "accomplished" : null
+                }`}
+              >
+                Finish
+              </div>
+            )}
+          </Step>
+        </ProgressBar>
+        <div className="py-5 flex">
+          <div>
+            Start
+          </div>
+          <div>
+            On Progress
+          </div>
+          <div>
+            Finish
+          </div>
+        </div>
+
+      </div>
       {/* time Progress section */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2 lg:gap-6 my-2">
         <Card className="">
